15130359f76f763a75ff6fa84e1c53d3a02fb979
kontr0x <55395652+kontr0x@users.noreply.github.com>
2024-07-01 23:01:14 +0200

    chore: Add zsh plugins for fzf, zsh-autocomplete, and zsh-autosuggestions
    This commit updates the zsh apply script to add three new plugins: fzf-zsh-plugin, zsh-autocomplete, and zsh-autosuggestions. The plugins are installed using the __install_omz_plugin function, which clones the respective GitHub repositories and adds them to the list of plugins in the .zshrc file.

--- zsh/apply.sh
+++ zsh/apply.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function __install_omz_plugin() {
+	git_repo=$1
+	plugin_name=$(sed -E 's/.*\/([a-zA-Z\-]+)\.git/\1/g' <<< $git_repo)
+	dst=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/$plugin_name
+	if [ ! -d $dst ]; then
+		git clone --quiet --depth 1 $git_repo $dst
+	fi
+	if ! grep -q -E 'plugins=\((.*'$plugin_name'.*)\)' ~/.zshrc; then
+		sed -i --follow-symlinks -E 's/plugins=\(([a-zA-Z \-]*)\)/plugins=(\1 '$plugin_name')/g' ~/.zshrc
+	fi
+}
+
 # Backup and link zshrc
 dst=$HOME/.zshrc
 if [ -f $dst ]; then
@@ -16,13 +28,8 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-# Check if fzf is installed and install fzf plugin
-dst=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-if command -v fzf &>/dev/null; then
-	if [ ! -d $dst ]; then
-		git clone --quiet --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git $dst
-	fi
-	sed -i -E 's/plugins=\(([a-zA-Z ]*)\)/plugins=(\1 fzf)/g' $HOME/.zshrc
-fi
+__install_omz_plugin https://github.com/unixorn/fzf-zsh-plugin.git
+__install_omz_plugin https://github.com/marlonrichert/zsh-autocomplete.git
+__install_omz_plugin https://github.com/zsh-users/zsh-autosuggestions.git
 
 exit 0
