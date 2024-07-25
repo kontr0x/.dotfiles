280765b5e507803b84df82bbe032e4a356b70e3d
kontr0x <55395652+kontr0x@users.noreply.github.com>
2024-07-25 02:22:53 +0200

    chore: Improve install.sh script

--- install.sh
+++ install.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
 
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
+source ./utils.sh
 
-if [ ! -d "./.git" ]; then
-	echo "ERROR: Not inside a git repository"
-	exit 1
+## Check if the -f flag is present
+#
+if [ "$1" == "-f" ]; then
+	read -p "This will overwrite your current configuration. Are you sure? [y/N] " -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		exit 1
+	fi
+
+	export IGNORE_DOTFILES_CHECK=1
+else
+	export IGNORE_DOTFILES_CHECK=0
 fi
 
-if [[ "`git rev-parse --show-toplevel`" != *".dotfiles" ]]; then
-	echo "ERROR: Not inside the correct git repository"
-	exit 1
-fi
-
-if [[ "`git rev-parse --show-toplevel`" != "`pwd`" ]]; then
-	echo "ERROR: install.sh not called inside the git repository"
-	exit 1
+if [ $IGNORE_DOTFILES_CHECK -eq 0 ]; then
+	## Check if the script is being called from the correct directory
+	#
+	if [ ! -d "./.git" ]; then
+		echo "ERROR: Not inside a git repository"
+		exit 1
+	fi
+	#
+	if [[ "`git rev-parse --show-toplevel`" != *".dotfiles" ]]; then
+		echo "ERROR: Not inside the correct git repository"
+		exit 1
+	fi
+	#
+	if [[ "`git rev-parse --show-toplevel`" != "`pwd`" ]]; then
+		echo "ERROR: install.sh not called inside the git repository"
+		exit 1
+	fi
 fi
 
 function apply_conf () {
 	if command -v $1 &>/dev/null; then
-		echo -e "[${GREEN}+${ENDCOLOR}] $1"
+		positive $1
 		cd $HOME/.dotfiles/$1
-		./apply.sh || echo -e "[${RED}-${ENDCOLOR}] $1"
+		./apply.sh || negative $1
 		cd ..
 	fi
 }
