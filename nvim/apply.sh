7b7e239ab30e8de6f663c91903fd4876e39e5b0b
kontr0x <55395652+kontr0x@users.noreply.github.com>
2025-06-09 01:08:22 +0200

    feat(nvim/apply): Add installation script for  Nerd Font

--- nvim/apply.sh
+++ nvim/apply.sh
@@ -10,4 +10,22 @@ if [ ! -d $dst ]; then
     clone_git_repo https://github.com/kontr0x/my-kickstart.nvim.git $HOME/.config/nvim
 fi
 
+## Install Hasklig Nerd Font
+#
+nerd_font_base_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
+nerd_font="Hasklig"
+#
+wget -q -O /tmp/${nerd_font}.zip ${nerd_font_base_url}${nerd_font}.zip
+if [ $? -ne 0 ]; then
+	negative "> Failed to download nerd font ${nerd_font}.zip"
+fi
+#
+unzip -o /tmp/${nerd_font}.zip -d $HOME/.fonts "*.otf"
+if [ $? -ne 0 ]; then
+	negative "> Failed to unzip nerd font ${nerd_font}.zip"
+fi
+
+fc-cache -fv $HOME/.fonts
+positive "> Successfully installed ${nerd_font} Nerd Font"
+
 exit 0
